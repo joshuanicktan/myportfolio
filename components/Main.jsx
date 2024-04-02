a70a3b70600a967b0aebfd11bcdcc3bba97bee1b
joshuanicktan <96874430+joshuanicktan@users.noreply.github.com>
2024-04-02 03:38:36 +0000

    Build Nextjs

--- components/Main.jsx
+++ components/Main.jsx
@@ -10,19 +10,19 @@ const Main = () => {
       <div className='max-w-[1240px] w-full h-full mx-auto p-2 flex justify-center items-center'>
         <div>
           <p className='uppercase text-sm tracking-widest text-gray-600'>
-            LET&#39;S BUILD SOMETHING TOGETHER
+            Hello there, I'm
           </p>
           <h1 className='py-4 text-gray-700'>
-            Hi, I&#39;m <span className='text-[#5651e5]'> Clint</span>
+            Joshua Nick Tan
           </h1>
-          <h1 className='py-2 text-gray-700'>A Front-End Web Developer</h1>
+          <h1 className='py-2 text-gray-700'>Robotics Engineering Student</h1>
           <p className='py-4 text-gray-600 sm:max-w-[70%] m-auto'>
-            I’m focused on building responsive front-end web applications
-            integrating back-end technologies.
+            SUTD Class of 2025
+
           </p>
           <div className='flex items-center justify-between max-w-[330px] m-auto py-4'>
             <a
-              href='https://www.linkedin.com/in/clint-briley-50056920a/'
+              href='https://www.linkedin.com/in/joshuanicktan/'
               target='_blank'
               rel='noreferrer'
             >
@@ -31,7 +31,7 @@ const Main = () => {
               </div>
             </a>
             <a
-              href='https://github.com/fireclint'
+              href='https://github.com/joshuanicktan'
               target='_blank'
               rel='noreferrer'
             >
